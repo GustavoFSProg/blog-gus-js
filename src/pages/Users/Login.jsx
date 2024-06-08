2e669858dbf745788975a6c84aa2b44d8d795343
OEM Configuration (temporary user) <gustavo.prog40@gmail.com>
2024-06-08 09:16:13 -0300

    react-router-dom routes

---
+++ src/pages/Users/Login.jsx
@@ -0,0 +1,14 @@
+function Login() {
+  return (
+    <>
+      <div style={{display: 'flex', width: '100vw', height: '100vh', alignItems: 'center',
+          justifyContent: 'center'
+
+      }}>
+        <h1>LOGIN</h1>
+      </div>
+    </>
+  );
+}
+
+export default Login;
---
+++ src/routes.jsx
@@ -0,0 +1,19 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import App from './App';
+import Login from './pages/Users/Login';
+
+function Routers() {
+    return (
+      <>
+       <BrowserRouter>
+         <Routes>
+            <Route path="/" exact element={<App />}/>
+            <Route path="/login"  element={<Login />}/>
+         </Routes>
+       </BrowserRouter>
+      </>
+    );
+  }
+  
+  export default Routers;
+  
