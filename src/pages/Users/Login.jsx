d110e03855961aaaefc17f90d297d9183657b6a8
OEM Configuration (temporary user) <gustavo.prog40@gmail.com>
2024-06-10 09:56:48 -0300

    token

--- src/pages/Users/Login.jsx
+++ src/pages/Users/Login.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import { Input } from "../../components/Input";
-import styled from 'styled-components'
+import styled from "styled-components";
 import NavBar from "../../components/NavBar/NavBar";
 import api from "../../api";
 
 const Button = styled.button`
-display: flex;
-align-items: center;
-justify-content: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   width: 107%;
   height: auto;
   background: #5c6b60;
@@ -20,156 +20,133 @@ justify-content: center;
   cursor: pointer;
   font-size: 16px;
 
-  &:hover{
+  &:hover {
     background: #7a9180;
     color: yellow;
   }
 
-  @media screen and (max-width: 800px){
-          width: 79.4vw;
-          margin-left: 33px; 
-        }
-
-`
+  @media screen and (max-width: 800px) {
+    width: 79.4vw;
+    margin-left: 33px;
+  }
+`;
 
 const Form = styled.form`
-display: flex;
- width: 100%;
- height: auto;
-        align-items: center;
-        justify-content: center;
-        flex-direction: column; 
+  display: flex;
+  width: 100%;
+  height: auto;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
 
-        @media screen and (max-width: 800px){
-          width: 72vw;
-          margin-left: -27px; 
-        }
-
-
-`
+  @media screen and (max-width: 800px) {
+    width: 72vw;
+    margin-left: -27px;
+  }
+`;
 
 const LoginContainer = styled.div`
-
-display: flex; 
- width: 27%;
+  display: flex;
+  width: 27%;
   height: auto;
-         align-items: center;
-         justify-content: center;
-         flex-direction: column;
-         
-         @media screen and (max-width: 800px){
-          margin-top: -90px;
-         }
- `
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  @media screen and (max-width: 800px) {
+    margin-top: -90px;
+  }
+`;
 
 function Login() {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  async function UserLogin(event){
-    event.preventDefault()
+  async function UserLogin(event) {
+    event.preventDefault();
 
     try {
-          const { data } = await api.post('/login', { email, password})
-    
-          // }
-          console.log(`TOKEN: ${data.token}`)
+      const { data } = await api.post("/login", { email, password });
 
-          if(!data.data){
-            return alert("Erro no Login preencha os campos!!")
-          }else{
+      // }
+      // console.log(`TOKEN: ${data.token}`)
 
-            return alert('Login  realizado com sucesso!')
-            }
-
-            
-          
-        } catch (error) {
-          return alert(`Erro no Login ${error}`)
-        }
+      if (!data.data) {
+        return alert("Erro no Login preencha os campos!!");
+      } else {
+        return alert("Login  realizado com sucesso!");
       }
+    } catch (error) {
+      return alert(`Erro no Login ${error}`);
+    }
+  }
 
-  
   return (
     <>
-      <div style={{display: 'flex',
-  background: 'lightgray',
-      
-      flexDirection: 'column', width: '100vw', height: '100vh', alignItems: 'center',
-          // justifyContent: 'center'
+      <div
+        style={{
+          display: "flex",
+          background: "lightgray",
 
-      }}>
+          flexDirection: "column",
+          width: "100vw",
+          height: "100vh",
+          alignItems: "center",
+        }}
+      >
         <NavBar />
 
-        <LoginContainer 
+        <LoginContainer>
+          <h1 style={{ marginTop: "150px" }}>LOGIN</h1>
+          <Form onSubmit={UserLogin}>
+            <div
+              style={{
+                display: "flex",
+                width: "100%",
+                height: "5rem",
+                alignItems: "center",
+                justifyContent: "center",
+                flexDirection: "column",
+                marginTop: "24px",
+              }}
+            >
+              <Input
+                type="email"
+                placeholder="email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                invalid={true}
+                // errorMessage="Email inválido"
+              />
+            </div>
 
+            <div
+              style={{
+                display: "flex",
+                width: "100%",
+                height: "5rem",
+                alignItems: "center",
+                justifyContent: "center",
+                flexDirection: "column",
+                marginTop: "-3px",
+              }}
+            >
+              <Input
+                type="password"
+                placeholder="senha"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                invalid={true}
+                // errorMessage="password inválido"
+              />
+            </div>
 
-        >
-
-        <h1 style={{marginTop: '150px'}}>LOGIN</h1>
-        <Form 
-       
-        onSubmit={UserLogin}>
-     
- <div style={{
-  display: 'flex', width: '100%', height: '5rem',
-  alignItems: 'center',
-  justifyContent: 'center',
-  flexDirection: 'column', 
-  marginTop: '24px',
-
-
-
-        }}> 
-
-
-
-        <Input
-            type="email"
-            placeholder="email"
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-            invalid={true}
-            // errorMessage="Email inválido"
-            />
-
-        </div>
-
-
-        <div style={{
-  display: 'flex', width: '100%', height: '5rem',
-  alignItems: 'center',
-  justifyContent: 'center',
-  flexDirection: 'column', 
-  marginTop: '-3px',
-
-
-        }}> 
-
-
-            <Input
-            type="password"
-            placeholder="senha"
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-            invalid={true}
-            // errorMessage="password inválido"
-            />
-
-</div>
-
-
-
-
-            <Button type="submit">
-              LOGAR
-
-            </Button>
-            </Form>
-      </LoginContainer>
+            <Button type="submit">LOGAR</Button>
+          </Form>
+        </LoginContainer>
       </div>
     </>
-  )
+  );
 }
 
 export default Login;
